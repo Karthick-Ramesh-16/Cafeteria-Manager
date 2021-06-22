ab1b692ad6b5644e24d2e5f3360fdf6ea4d599f2
Karthick-sketch <karthick246.kh@outlook.com>
2021-06-22 23:12:24 +0530

    Added cookies session to stay user logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,11 @@
 class HomeController < ApplicationController
-  def index
-    render "index"
-  end
+  skip_before_action :ensure_user_logged_in
 
-  def create
+  def index
+    if current_user
+      redirect_to menu_categories_path
+    else
+      render "index"
+    end
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,14 @@
 class SessionsController < ApplicationController
+  skip_before_action :ensure_user_logged_in
+
   def new
   end
 
   def create
     user = User.find_by(email: params[:email])
     if user and user.authenticate(params[:password])
-      render plain: user.first_name
+      session[:current_user_id] = user.id
+      redirect_to menu_categories_path
     else
       redirect_to new_sessions_path
     end
