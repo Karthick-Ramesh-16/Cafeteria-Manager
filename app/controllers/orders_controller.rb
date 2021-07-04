78c336851895ae56c29dfd8aeb71dbe21aed5210
Karthick-sketch <karthick246.kh@outlook.com>
2021-07-04 13:07:49 +0530

    Now owner can set user delivery as delivered

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   def index
-    @orders = @current_user.orders
+    @orders = (@current_user.role == "owner") ? Order.all : @current_user.orders
     render "index"
   end
 
@@ -12,5 +12,13 @@ class OrdersController < ApplicationController
     )
 
     redirect_to create_order_item_path(id: order.id)
+  end
+
+  def update
+    order = Order.find(params[:id])
+    order.delivered_at = DateTime.now
+    order.save!
+
+    redirect_to orders_path
   end
 end
