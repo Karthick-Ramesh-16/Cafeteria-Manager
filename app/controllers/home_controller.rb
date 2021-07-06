5b2840121313cbb304d0ffe2d9131f5a0f4789f1
Karthick-sketch <karthick246.kh@outlook.com>
2021-07-07 01:34:58 +0530

    Render Sign Up page instead Home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,6 @@ class HomeController < ApplicationController
   skip_before_action :ensure_user_logged_in
 
   def index
-    if current_user
-      redirect_to menu_categories_path
-    else
-      render "index"
-    end
+    redirect_to (current_user ? menu_categories_path : new_sessions_path)
   end
 end
