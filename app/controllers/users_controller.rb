b218ea66d0e0270cd28f1940c82666c10a190a2b
Karthick-sketch <karthick246.kh@outlook.com>
2021-07-07 14:22:39 +0530

    Render 404 for all users file and use chennai time zone

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     if @current_user.role == "Owner"
       render "index"
     else
-      redirect_back(fallback_location: "/")
+      render :file => "#{Rails.root}/public/404.html", layout: false, status: :not_found
     end
   end
 
